7022ce286cd957163e7133771de50b1612d55bf0
shijia2118 <228749551@qq.com>
2022-08-28 15:11:23 +0800

    no message

--- lib/ys_play_view.dart
+++ lib/ys_play_view.dart
@@ -4,14 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:ys_play/constants.dart';
 
 class YsPlayView extends StatelessWidget {
-  const YsPlayView({Key? key}) : super(key: key);
+  final Map<String,dynamic> creationParams;
+  const YsPlayView({Key? key,required this.creationParams,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (Platform.isIOS) {
-      return  UiKitView(viewType: Constants.METHOD_CHANNEL);
+      return  UiKitView(viewType: Constants.METHOD_CHANNEL,creationParams: creationParams,);
     } else if (Platform.isAndroid) {
-      return  AndroidView(viewType: Constants.METHOD_CHANNEL);
+      return  AndroidView(viewType: Constants.METHOD_CHANNEL,creationParams: creationParams,);
     } else {
       throw UnimplementedError();
     }
