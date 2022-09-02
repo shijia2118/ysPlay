f35a82edd3686fed69e4f8d241a7c6c2ed713688
shijia2118 <228749551@qq.com>
2022-09-02 16:02:36 +0800

    no message

--- ios/Classes/SwiftYsPlayPlugin.swift
+++ ios/Classes/SwiftYsPlayPlugin.swift
@@ -1,33 +1,14 @@
 import Flutter
 import UIKit
-import EZOpenSDKFramework
 
 public class SwiftYsPlayPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-
-    let factory = EZUIPlayerViewFactory(messenger: registrar.messenger())
-    registrar.register(factory, withId: "com.example.ys_play")
-
-    let channel = FlutterMethodChannel(name: "com.example.ys_play", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "ys_play", binaryMessenger: registrar.messenger())
     let instance = SwiftYsPlayPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
-   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if call.method == "init_sdk" {
-        let data:Optional<Dictionary> = call.arguments as! Dictionary<String, String>
-
-        result(EZOpenSDK.initLib(withAppKey: data?["appKey"]))
-    } else if call.method == "set_access_token" {
-        let data:Optional<Dictionary> = call.arguments as! Dictionary<String, String>
-        EZOpenSDK.setAccessToken(data?["accessToken"] as! String)
-        result("success")
-    } else if call.method == "destoryLib" {
-        EZOpenSDK.destoryLib()
-        result("success")
-    } else {
-        result(FlutterMethodNotImplemented)
-    }
-
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    result("iOS " + UIDevice.current.systemVersion)
   }
 }
