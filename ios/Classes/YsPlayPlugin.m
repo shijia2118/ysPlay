f9d3a2123b48e9e5f9550e25a8a54d0408770b06
shijia2118 <228749551@qq.com>
2022-09-05 09:16:46 +0800

    no message

--- ios/Classes/YsPlayPlugin.m
+++ ios/Classes/YsPlayPlugin.m
@@ -1,15 +1,20 @@
 #import "YsPlayPlugin.h"
-#if __has_include(<ys_play/ys_play-Swift.h>)
-#import <ys_play/ys_play-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "ys_play-Swift.h"
-#endif
 
 @implementation YsPlayPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftYsPlayPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"ys_play"
+            binaryMessenger:[registrar messenger]];
+  YsPlayPlugin* instance = [[YsPlayPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
