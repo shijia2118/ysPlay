ddd70fca91478d3f1b31586f5e2e85ba974d70bf
shijia2118 <228749551@qq.com>
2022-12-16 23:31:26 +0800

    no message

--- android/src/main/java/com/example/ys_play/utils/LogUtils.java
+++ android/src/main/java/com/example/ys_play/utils/LogUtils.java
@@ -2,13 +2,13 @@ package com.example.ys_play.utils;
 
 import android.util.Log;
 
-import com.example.ys_play.BuildConfig;
+import io.flutter.BuildConfig;
 
 public class LogUtils {
     static String TAG =  "萤石LOG========>";
 
     public static void d(String text){
-        if(BuildConfig.DEBUG&&text!=null){
+        if(BuildConfig.DEBUG && text!=null){
             Log.d(TAG,text);
         }
     }
